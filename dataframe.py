085a3ce52297988bfe6b034ef4be748ae82dda71
sekky0905 <ks.sekiguchi.0905@gmail.com>
2017-11-25 20:14:17 +0900

    dataframeで後からindexとcolumnをつけてみた

--- dataframe.py
+++ dataframe.py
@@ -1,6 +1,7 @@
 # pandasをimport
 import pandas as pd
 
+print("=====create DataFrame from dictionary=====")
 # dictionaryを作成する
 dictionary = {
     'Name': ["Python", "Go", "java"],
@@ -14,4 +15,15 @@ df = pd.DataFrame(dictionary).T
 print("=====DataFrame=====")
 print(df)
 
+print("=====create DataFrame=====")
+df2 = pd.DataFrame([["Python", "Go", "java"],
+        ["Dynamic", "Static", "Static"],
+        ]).T
+print("=====DataFrame2=====")
+print(df2)
 
+df2.columns = ["Name", "Type"]
+df2.index = ["0", "1", "2"]
+
+print("=====DataFrame2 with columns and index=====")
+print(df2)
